6b3963266e533fb745aac99e5ec08cb2066262fc
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-04-12 10:09:46 +0200

    File upload field

---
+++ views/fields/file_upload.blade.php
@@ -0,0 +1,25 @@
+<div class="grid">
+    <div class="grid-md-12">
+        <div class="form-group">
+            <label for="{{ $module_id }}-{{ sanitize_title($field['label']) }}">{{ $field['label'] }}{!!  $field['required'] ? '<span class="text-danger">*</span>' : '' !!}</label>
+
+            @if ($field['type'] === 'multiple')
+            <ul class="input-files" data-max="{{ $field['files_max'] }}">
+                <li>
+                    <label class="input-file">
+                        <input type="file" name="file[]" {{ $field['filetypes'] && is_array($field['filetypes']) ? 'accept="' . implode(',', $field['filetypes']) . '"' : '' }} {{ $field['required'] ? 'required' : '' }}>
+                        <span class="btn"><?php _e('Select file', 'modularity-form-builder'); ?></span>
+                        <span class="input-file-selected"><?php _e('No file selected', 'modularity-form-builder'); ?></span>
+                    </label>
+                </li>
+            </ul>
+            @else
+                <label class="input-file">
+                    <input type="file" name="file[]" {{ $field['filetypes'] && is_array($field['filetypes']) ? 'accept="' . implode('|', $field['filetypes']) . '"' : '' }} {{ $field['required'] ? 'required' : '' }}>
+                    <span class="btn"><?php _e('Select file', 'modularity-form-builder'); ?></span>
+                    <span class="input-file-selected"><?php _e('No file selected', 'modularity-form-builder'); ?></span>
+                </label>
+            @endif
+        </div>
+    </div>
+</div>
