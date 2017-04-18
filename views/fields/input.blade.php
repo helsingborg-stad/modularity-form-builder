602b8f97ce09e678accc07c13e90007e806e1852
Kristoffer Svanmark <kristoffer.svanmark@knowit.se>
2017-04-18 09:52:30 +0200

    Input field fix

--- views/fields/input.blade.php
+++ views/fields/input.blade.php
@@ -7,6 +7,8 @@
                 <input type="{{ $field['value_type'] }}" id="{{ $module_id }}-input-{{ sanitize_title($field['label']) }}" name="{{ sanitize_title($field['label']) }}" {{  $field['required'] ? 'required' : '' }} {!! $field['min_value'] ? 'min="' . $field['min_value'] . '"' : '' !!} {!! $field['max_value'] ? 'max="' . $field['max_value'] . '"' : '' !!} {!! $field['step'] ? 'step="' . $field['step'] . '"' : '' !!}>
             @elseif ($field['value_type'] === 'date')
                 <input type="{{ $field['value_type'] }}" id="{{ $module_id }}-input-{{ sanitize_title($field['label']) }}" name="{{ sanitize_title($field['label']) }}" {{  $field['required'] ? 'required' : '' }} {!! $field['min_value'] ? 'min="' . $field['min_value'] . '"' : '' !!} {!! $field['max_value'] ? 'max="' . $field['max_value'] . '"' : '' !!}>
+            @else
+                <input type="{{ $field['value_type'] }}" id="{{ $module_id }}-input-{{ sanitize_title($field['label']) }}" name="{{ sanitize_title($field['label']) }}" {{  $field['required'] ? 'required' : '' }}>
             @endif
         </div>
     </div>
