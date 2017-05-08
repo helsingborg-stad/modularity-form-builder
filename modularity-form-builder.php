d80ccd933282f3b8f6d7f1d2ac6766986a425913
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-05-08 13:14:49 +0200

    Run forms after all plugins is loaded.

--- modularity-form-builder.php
+++ modularity-form-builder.php
@@ -33,7 +33,7 @@ add_action('plugins_loaded', function () {
 /**
  * Registers the module
  */
-add_action('init', function () {
+add_action('plugins_loaded', function () {
     if (function_exists('modularity_register_module')) {
         modularity_register_module(
             FORM_BUILDER_MODULE_PATH,
