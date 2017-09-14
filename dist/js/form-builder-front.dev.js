669b8f81efd851d4c24fb3fa7e9d09657601e144
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-09-14 08:36:32 +0200

    Gulped

--- dist/js/form-builder-front.dev.js
+++ dist/js/form-builder-front.dev.js
@@ -12,10 +12,10 @@ FormBuilder.Front.handleConditions = (function ($) {
     handleConditions.prototype.handleEvents = function () {
         $('input[type=radio]', '.modularity-mod-form').change(function() {
             var conditional = $(this).attr('conditional');
-            if (typeof conditional == 'string') {
-                var conditions = conditional.split('[#]');
-                $('div[conditional-target^="' + conditions[0] + '"]').hide();
-                $('div[conditional-target^="' + conditions[0] + '"][conditional-target$="' + conditions[1] + '"]').show();
+            if (typeof conditional !== 'undefined' && conditional.length > 0) {
+                var conditionObj = JSON.parse(conditional);
+                $("div[conditional-target^='{\"label\":\"" + conditionObj.label + "\",']").hide();
+                $("div[conditional-target='" + conditional + "']").show();
             }
         });
     };
