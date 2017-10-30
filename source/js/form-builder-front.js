5c594ff698eb756e3a85eb8cd0977ee3e697147d
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-10-30 13:14:23 +0100

    Remove required attribute on hidden fields

--- source/js/form-builder-front.js
+++ source/js/form-builder-front.js
@@ -6,19 +6,30 @@ FormBuilder.Front = FormBuilder.Front || {};
 FormBuilder.Front.handleConditions = (function ($) {
 
     function handleConditions() {
-        this.handleEvents();
+        $(function() {
+            this.handleRequired();
+            this.handleEvents();
+        }.bind(this));
     }
+
+    handleConditions.prototype.handleRequired = function () {
+        $target = $('[class*="mod-form"]');
+        $('[conditional-target]:hidden', $target).find('[required]').prop('required', false).attr('hidden-required', true);
+        $('[conditional-target]:visible', $target).find('[hidden-required]').prop('required', true);
+    };
 
     handleConditions.prototype.handleEvents = function () {
         $('input[conditional]').change(function(e) {
             $target = $(e.target).parents('[class*="mod-form"]');
-            var conditional = $(this).attr('conditional');
+            var conditional = $(e.target).attr('conditional');
             if (typeof conditional !== 'undefined' && conditional.length > 0) {
                 var conditionObj = JSON.parse(conditional);
                 $target.find("div[conditional-target^='{\"label\":\"" + conditionObj.label + "\",']").hide();
                 $target.find("div[conditional-target='" + conditional + "']").show();
             }
-        });
+
+            this.handleRequired();
+        }.bind(this));
     };
 
 	return new handleConditions();
