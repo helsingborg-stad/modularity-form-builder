debe29d4921ce9830f61be020d182bedae3560c8
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-11-10 12:56:13 +0100

    Added missing Multiple attribute

--- views/fields/file_upload.blade.php
+++ views/fields/file_upload.blade.php
@@ -8,7 +8,7 @@
             <ul class="input-files" data-max="{{ $field['files_max'] }}">
                 <li>
                     <label class="input-file">
-                        <input type="file" name="{{ sanitize_title($field['label']) }}[]" {!! $field['filetypes'] && is_array($field['filetypes']) ? 'accept="' . implode(',', $field['filetypes']) . '"' : '' !!} {{ $field['required'] ? 'required' : '' }}>
+                        <input type="file" name="{{ sanitize_title($field['label']) }}[]" {!! $field['filetypes'] && is_array($field['filetypes']) ? 'accept="' . implode(',', $field['filetypes']) . '"' : '' !!} {{ $field['required'] ? 'required' : '' }} multiple>
                         <span class="btn"><?php _e('Select file', 'modularity-form-builder'); ?></span>
                         <span class="input-file-selected"><?php _e('No file selected', 'modularity-form-builder'); ?></span>
                     </label>
