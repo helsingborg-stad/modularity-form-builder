e45dda7ee91cdc868005887b561b9b0948b084ab
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-11-17 14:11:48 +0100

    Gulp

--- dist/js/form-builder-admin.dev.js
+++ dist/js/form-builder-admin.dev.js
@@ -1,5 +1,3 @@
-var FormBuilder = FormBuilder || {};
-
 FormBuilder = FormBuilder || {};
 FormBuilder.Admin = FormBuilder.Admin || {};
 
@@ -122,5 +120,29 @@ FormBuilder.Admin.Conditional = (function ($) {
     };
 
 	return new Conditional();
+
+})(jQuery);
+
+var FormBuilder = FormBuilder || {};
+
+FormBuilder = FormBuilder || {};
+FormBuilder.Admin = FormBuilder.Admin || {};
+
+FormBuilder.Admin.various = (function ($) {
+
+    function various() {
+        $(function() {
+            this.externalUpload();
+        }.bind(this));
+    }
+
+    // Show/hide external upload field
+    various.prototype.externalUpload = function () {
+    	if (!formbuilder.mod_form_authorized) {
+    		$('[data-name="upload_videos_external"]').hide();
+    	}
+    };
+
+	return new various();
 
 })(jQuery);
