b7dc97e1c97f4d9b3ee3abfd820e374f1a00d758
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-11-24 15:21:27 +0100

    Display file urls correctly

--- source/php/Module/views/admin/formdata.php
+++ source/php/Module/views/admin/formdata.php
@@ -1,8 +1,8 @@
-<?php foreach ($data as $item) : ?>
-    <p data-type="<?php echo $item['type']; ?>">
+<?php foreach ($data['form_fields'] as $item) : ?>
+    <p data-type="<?php echo $item['acf_fc_layout']; ?>">
         <strong><?php echo $item['label']; ?></strong><br>
         <?php
-        if ($item['type'] === 'file_upload' && is_array($item['value'])) {
+        if ($item['acf_fc_layout'] === 'file_upload' && is_array($item['value'])) {
             $i = 0;
             $uploadFolder = wp_upload_dir();
             $uploadFolder = $uploadFolder['baseurl'] . '/modularity-form-builder/';
@@ -10,7 +10,9 @@
             foreach ($item['value'] as $file) {
                 $i++;
                 if ($i > 1) echo '<br>';
-                echo 'Öppna fil: <a target="_blank" href="' . $uploadFolder . basename($file) . '">' . basename($file) . '</a>';
+                $filePath = file_exists($file) ? $uploadFolder . basename($file) : $file;
+                $fileName = file_exists($file) ? basename($file) : $file;
+                echo 'Öppna fil: <a target="_blank" href="' . $filePath . '">' . $fileName . '</a>';
             }
         } elseif (is_array($item['value'])) {
             foreach ($item['value'] as $value) {
