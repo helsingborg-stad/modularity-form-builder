930dfe262e8619e3c0d0681199de10805287fa4a
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-12-05 15:41:35 +0100

    Gulped

--- dist/js/form-builder-front.dev.js
+++ dist/js/form-builder-front.dev.js
@@ -1,3 +1,39 @@
+FormBuilder = FormBuilder || {};
+FormBuilder.Front = FormBuilder.Front || {};
+
+FormBuilder.Front.collapse = (function ($) {
+
+    function collapse() {
+        $(function() {
+            this.init();
+            this.handleEvents();
+        }.bind(this));
+    }
+
+    collapse.prototype.init = function() {
+        $('.mod-form-collapse').each(function(index) {
+            $(this).nextUntil(':not(.mod-form-field)').hide();
+        });
+    };
+
+    collapse.prototype.handleEvents = function () {
+        $('button', '.mod-form-collapse').click(function(e) {
+            e.preventDefault();
+
+            $('.mod-form-collapse__icon > i', e.target).toggleClass('pricon-plus-o pricon-minus-o');
+
+            $(e.target).parents('.mod-form-collapse')
+                .nextUntil(':not(.mod-form-field)')
+                .each(function() {
+                    $(this).fadeToggle('fast');
+                });
+
+        }.bind(this));
+    };
+
+    return new collapse();
+})(jQuery);
+
 var FormBuilder = FormBuilder || {};
 
 FormBuilder = FormBuilder || {};
