490d99ffde300ce2742ce72013c8c5e0c449b277
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-12-05 15:34:54 +0100

    Collapse button markup

---
+++ source/php/Module/views/fields/collapse.blade.php
@@ -0,0 +1,6 @@
+<div class="mod-form-collapse" {!! $field['conditional_hidden'] !!}>
+    <button class="btn btn-lg btn-block">
+        <span class="mod-form-collapse__text">{{ $field['button_text'] }}</span>
+        <span class="mod-form-collapse__icon"><i class="pricon pricon-plus-o"></i></span>
+    </button>
+</div>
