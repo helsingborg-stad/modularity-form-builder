54a73c565f548f36c281223faccb439e6ed79194
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-12-05 15:33:29 +0100

    Changed scss file names

--- gulpfile.js
+++ gulpfile.js
@@ -12,17 +12,17 @@ var plumber = require('gulp-plumber');
 
 // Compile Our Sass
 gulp.task('sass-dist', function() {
-    gulp.src('source/sass/todo.scss')
-            .pipe(plumber())
-            .pipe(sass())
-            .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(cssnano())
-            .pipe(gulp.dest('dist/css'));
+    gulp.src('source/sass/modularity-form-builder.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('sass-dev', function() {
-    gulp.src('source/sass/todo.scss')
+    gulp.src('source/sass/modularity-form-builder.scss')
         .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
