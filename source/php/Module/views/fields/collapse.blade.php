3a50043a6d6d453c38dc628e8e935a6d4f76f0c4
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-03-21 13:16:40 +0100

    Added padding to collapse

--- source/php/Module/views/fields/collapse.blade.php
+++ source/php/Module/views/fields/collapse.blade.php
@@ -1,4 +1,4 @@
-<div class="mod-form-collapse" {!! $field['conditional_hidden'] !!}>
+<div class="mod-form-collapse gutter gutter-top" {!! $field['conditional_hidden'] !!}>
     <button class="btn btn-lg btn-block">
         <span class="mod-form-collapse__text">{{ $field['button_text'] }}</span>
         <span class="mod-form-collapse__icon"><i class="pricon pricon-plus-o"></i></span>
