8f0b5d2a5311beecaa13cec3f0ab2f7ad8a01288
Max Coles Frederiksen <max@colesmedia.se>
2019-12-05 09:06:49 +0100

    added target group to post again

--- source/php/Submission.php
+++ source/php/Submission.php
@@ -115,11 +115,16 @@ class Submission
         }
 
         $name = $userMeta['name_of_council_or_politician'][0];
+        $targetGroup = $userMeta['target_group'][0];
+        $postFormData = get_post_meta($submission, 'form-data')[0];
+        $subjects = $postFormData['valj-amnen-kategorier'];
 
         update_post_meta($submission, 'modularity-form-id', $_POST['modularity-form-id']);
         update_post_meta($submission, 'modularity-form-referer', strtok($referer, '?'));
-        if (!empty($name)) {
+        if (!empty($name) && !empty($targetGroup)) {
+            update_post_meta($submission, 'subjects', $subjects);
             update_post_meta($submission, 'name_of_council_or_politician', $name);
+            update_post_meta($submission, 'target_group', $targetGroup);
         }
 
         //The form url
