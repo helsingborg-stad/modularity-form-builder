06f271a5654d0b626cedacef13e057ebd754893d
Max Frederiksen <max@colesmedia.se>
2020-01-10 14:31:02 +0100

    added errors div

--- source/php/Module/views/fields/checkbox.blade.php
+++ source/php/Module/views/fields/checkbox.blade.php
@@ -4,6 +4,8 @@
             <label for="{{ $module_id }}-{{ sanitize_title($field['label']) }}">{{ $field['label'] }}{!!  $field['required'] ? '<span class="text-danger">*</span>' : '' !!}</label>
             {!! !empty($field['description']) ? '<div class="text-sm text-dark-gray">' . ModularityFormBuilder\Helper\SanitizeData::convertLinks($field['description']) . '</div>' : '' !!}
 
+            <div class="errors"></div>
+
             @foreach ($field['values'] as $value)
                 <label class="checkbox">
                     <input type="checkbox" class="{{ $field['required'] ? 'required' : '' }}" name="{{ sanitize_title($field['label']) }}[]" value="{{ $value['value'] }}"> {{ $value['value'] }}
