d963d8f3719191b80742bd09868c7da6dd921c1d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-05-11 07:42:12 +0200

    Reset conf file

--- acf-fields/php/form.php
+++ acf-fields/php/form.php
@@ -1,7 +1,9 @@
 <?php 
 
-if (function_exists('acf_add_local_field_group')) {
-    acf_add_local_field_group(array(
+
+if (function_exists('acf_add_local_field_group')) {
+
+    acf_add_local_field_group(array(
     'key' => 'group_58eb301ecb36a',
     'title' => 'Form',
     'fields' => array(
@@ -2630,4 +2632,5 @@
     'active' => 1,
     'description' => '',
 ));
-}
+
+}
