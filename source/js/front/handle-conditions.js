49b77c7ac62e9db952a0dd040152151464f7fc3e
Alexander Boman Skoug <alexanderbomanskoug@gmail.com>
2020-06-23 11:12:32 +0200

    fixed varaible names

--- source/js/front/handle-conditions.js
+++ source/js/front/handle-conditions.js
@@ -5,19 +5,19 @@ export default (function ($) {
     }
 
     HandleConditions.prototype.handleRequired = function () {
-        const $target = $('[class*="mod-form"]');
-        $('[conditional-target]:hidden', $target).find('[required]').prop('required', false).attr('hidden-required', true);
-        $('[conditional-target]:visible', $target).find('[hidden-required]').prop('required', true);
+        const target = $('[class*="mod-form"]');
+        $('[conditional-target]:hidden', target).find('[required]').prop('required', false).attr('hidden-required', true);
+        $('[conditional-target]:visible', target).find('[hidden-required]').prop('required', true);
     };
 
     HandleConditions.prototype.handleEvents = function () {
         $('input[conditional]').change(function(e) {
-            const $target = $(e.target).parents('[class*="mod-form"]');
+            const target = $(e.target).parents('[class*="mod-form"]');
             var conditional = $(e.target).attr('conditional');
             if (typeof conditional !== 'undefined' && conditional.length > 0) {
                 var conditionObj = JSON.parse(conditional);
-                $target.find("div[conditional-target^='{\"label\":\"" + conditionObj.label + "\",']").hide();
-                $target.find("div[conditional-target='" + conditional + "']").show();
+                target.find("div[conditional-target^='{\"label\":\"" + conditionObj.label + "\",']").hide();
+                target.find("div[conditional-target='" + conditional + "']").show();
             }
 
             this.handleRequired();
