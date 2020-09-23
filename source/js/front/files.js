98bd6bb00f8f8ceeeed9dc636a71531c433dfa9a
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-09-23 12:53:39 +0200

    Select all forms

--- source/js/front/files.js
+++ source/js/front/files.js
@@ -1,16 +1,28 @@
 export default (function addFile() {
-    const visibileInput = document.querySelector('.c-fileinput__input');
+    const forms = document.querySelectorAll('.mod-form');
+    
+    forms.forEach((form) => {
+        
+        const visibileInput = form.querySelector('.c-fileinput__input');
+        const submit = form.querySelector('[type="submit"]');
 
-    visibileInput.addEventListener('change', (event) => {
-        const files = visibileInput.files;
-        const hiddenInput = visibileInput.cloneNode(true);
-        hiddenInput.setAttribute('style', 'display:none');
-        visibileInput.parentNode.insertBefore(hiddenInput, visibileInput.nextSibling);
-        visibileInput.value = '';
-        //visibileInput.files = [];
+        visibileInput.addEventListener('change', (event) => {
 
-        //console.log(visibileInput.files);
-        console.log(hiddenInput.files);
+            const hiddenInput = visibileInput.cloneNode(true);
+            hiddenInput.setAttribute('style', 'display:none');
+            visibileInput.parentNode.insertBefore(hiddenInput, visibileInput.nextSibling);
+            //visibileInput.value = '';
+            //visibileInput.files = [];
+            
+            //console.log(visibileInput.files);
+            console.log(hiddenInput.files);
+        });
+
+        console.log(submit);
+
+        submit.addEventListener('submit', (event) => {
+            console.log(event);
+        });
     });
-    console.log(visibileInput);
+    
 })();
