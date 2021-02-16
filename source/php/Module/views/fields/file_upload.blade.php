425fde13c745ccebdb15c59df62238511c26f60f
Eric Rosenborg <eric.rosenborg@hiq.se>
2021-02-16 09:21:12 +0100

    Add text displaying max number of files, fix accepted files, add an filesMax attribute

--- source/php/Module/views/fields/file_upload.blade.php
+++ source/php/Module/views/fields/file_upload.blade.php
@@ -10,9 +10,16 @@
                         'display' => 'area',
                         'multiple' => ($field['type'] === 'multiple') ? true : false,
                         'label' => translate('Select file', 'modularity-form-builder'),
-                        'accept' => $field['filetypes'] && is_array($field['filetypes']) ? 'accept="' . implode(',', $field['filetypes']) . '"' : ''
+                        'accept' => $field['filetypes'] && is_array($field['filetypes']) ? 'accept="' .$field['attributeList']['accept'] . '"' : '',
+                        'filesMax' => $field['files_max']
                     ])
                     @endfileinput
+
+                    @if(isset($field['files_max']))
+                        @typography(['element' => 'p', 'variant' => 'meta'])
+                            {{__('Max number of files', 'modularity-form-builder')}}: {{$field['files_max']}}
+                        @endtypography        
+                    @endif
 
                     <!-- <span class="input-file-selected"><?php _e('No file selected', 'modularity-form-builder'); ?></span> -->
 
