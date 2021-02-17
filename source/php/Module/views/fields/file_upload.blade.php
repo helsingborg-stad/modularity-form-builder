7377efd874563a176793e1e4e4f831294438805c
Eric Rosenborg <eric.rosenborg@hiq.se>
2021-02-17 10:57:55 +0100

    WIP

--- source/php/Module/views/fields/file_upload.blade.php
+++ source/php/Module/views/fields/file_upload.blade.php
@@ -11,13 +11,14 @@
                         'multiple' => ($field['type'] === 'multiple') ? true : false,
                         'label' => translate('Select file', 'modularity-form-builder'),
                         'accept' => $field['filetypes'] && is_array($field['filetypes']) ? 'accept="' .$field['attributeList']['accept'] . '"' : '',
-                        'filesMax' => $field['files_max']
+                        'filesMax' => ($field['type'] === 'multiple') ? $field['files_max'] : 1
                     ])
                     @endfileinput
 
                     @if(isset($field['files_max']))
                         @typography(['element' => 'p', 'variant' => 'meta'])
-                            {{__('Max number of files', 'modularity-form-builder')}}: {{$field['files_max']}}
+                            {{__('Max number of files', 'modularity-form-builder')}}: 
+                            {{($field['type'] === 'multiple' ? $field['files_max'] : '1')}}
                         @endtypography        
                     @endif
 
