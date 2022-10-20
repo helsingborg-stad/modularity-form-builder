581af3e37046671ed0f3b6db66433c288c08e8bb
Anna Johansson <anna@thehappyhipster.se>
2022-10-20 15:54:31 +0200

    Use standard WP translation of "Select file"

--- source/php/Module/views/fields/file_upload.blade.php
+++ source/php/Module/views/fields/file_upload.blade.php
@@ -1,28 +1,33 @@
-<div class="o-grid mod-form-field" {!! $field['conditional_hidden'] !!}>
+<div {!! $field['conditional_hidden'] !!} class="o-grid mod-form-field">
     <div class="o-grid-12@md">
         <div class="form-group">
-            <label for="{{ $module_id }}-{{ sanitize_title($field['label']) }}">{{ $field['label'] }}{!!  $field['required'] ? '<span class="text-danger">*</span>' : '' !!}</label>
-            {!! !empty($field['description']) ? '<div class="text-sm text-dark-gray">' . ModularityFormBuilder\Helper\SanitizeData::convertLinks($field['description']) . '</div>' : '' !!}
+            <label
+                for="{{ $module_id }}-{{ sanitize_title($field['label']) }}">{{ $field['label'] }}{!! $field['required'] ? '<span class="text-danger">*</span>' : '' !!}</label>
+            {!! !empty($field['description'])
+                ? '<div class="text-sm text-dark-gray">' .
+                    ModularityFormBuilder\Helper\SanitizeData::convertLinks($field['description']) .
+                    '</div>'
+                : '' !!}
 
 
-                    @fileinput([
-                        'name' => sanitize_title($field['label']),
-                        'display' => 'area',
-                        'multiple' => ($field['type'] === 'multiple') ? true : false,
-                        'label' => __('Select file', 'modularity-form-builder'),
-                        'accept' => $field['filetypes'] && is_array($field['filetypes']) ? $field['attributeList']['accept'] : '',
-                        'filesMax' => ($field['type'] === 'multiple') ? $field['files_max'] : 1
-                    ])
-                    @endfileinput
+            @fileinput([
+                'name' => sanitize_title($field['label']),
+                'display' => 'area',
+                'multiple' => $field['type'] === 'multiple' ? true : false,
+                'label' => __('Select file'),
+                'accept' => $field['filetypes'] && is_array($field['filetypes']) ? $field['attributeList']['accept'] : '',
+                'filesMax' => $field['type'] === 'multiple' ? $field['files_max'] : 1
+            ])
+            @endfileinput
 
-                    @if(isset($field['files_max']))
-                        @typography(['element' => 'p', 'variant' => 'meta'])
-                            {{__('Max number of files', 'modularity-form-builder')}}: 
-                            {{($field['type'] === 'multiple' ? $field['files_max'] : '1')}}
-                        @endtypography        
-                    @endif
+            @if (isset($field['files_max']))
+                @typography(['element' => 'p', 'variant' => 'meta'])
+                    {{ __('Max number of files', 'modularity-form-builder') }}:
+                    {{ $field['type'] === 'multiple' ? $field['files_max'] : '1' }}
+                @endtypography
+            @endif
 
-                    <!-- <span class="input-file-selected"><?php _e('No file selected', 'modularity-form-builder'); ?></span> -->
+            <!-- <span class="input-file-selected"><?php _e('No file selected', 'modularity-form-builder'); ?></span> -->
 
         </div>
     </div>
