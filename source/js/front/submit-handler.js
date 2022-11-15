639870e06428d0bf49f3cfdb2fcbf93aba8ad217
NiclasNorin <niclas.norin@frontwalker.se>
2022-11-15 11:14:08 +0100

    Moved logic to field.js

--- source/js/front/submit-handler.js
+++ source/js/front/submit-handler.js
@@ -4,11 +4,5 @@ export default (function ($) {
             this.handleEvents();
         }.bind(this));
     }
-    // Show spinner icon on submit
-    Submit.prototype.handleEvents = function () {
-        $('[class*="mod-form"]').submit(function (e) {
-            $(e.target).find('button[type="submit"]').html('<i class="spinner"></i> ' + formbuilder.sending);
-        }.bind(this));
-    };
     return new Submit();
 })(jQuery);
