1333b07ef50b4e00082d2991e83bb88d7a12f4b2
NiclasNorin <niclas.norin@frontwalker.se>
2022-11-22 15:48:39 +0100

    Checkbox validation

--- source/js/front/checkbox-required.js
+++ source/js/front/checkbox-required.js
@@ -1,54 +1,32 @@
 export default (() => {
 
-    /*
-    ** Will validate the checkbox for checkbox groups
-    */
-    const getCheckBoxGroups = () => {
-        document.querySelectorAll('.checkbox-group-required').forEach(group => {
-            const checkboxes = group.querySelectorAll('.c-option [type="checkbox"]:checked');
-            const validator = group.querySelector('.js-checkbox-valid');
-
-            // If there is checked boxes then tick off the validating checkbox
-            if (checkboxes.length > 0) {
-                validator.setAttribute('checked', true);
-            } else {
-                validator.removeAttribute('checked');
-            }
-        })
-    }
-
-    /*
-    ** Adds event listener for when a checkbox is change to revalidate form
-    */
+    /**
+     * When a checkbox is checked, add the "checked" attribute to the element (validationElement).
+     * If there are no checked checkboxes within a group, remove the attribute.
+     */
     const addListeners = () => {
-        document.querySelectorAll('.checkbox-group').forEach(group => {
-            const checkboxes = group.querySelectorAll('.c-option [type="checkbox"]');
+        document.querySelectorAll('.checkbox-group-required').forEach(checkboxGroup => {
+            const checkboxes = checkboxGroup.querySelectorAll('.c-option__checkbox--hidden-box');
+            let validationElement = checkboxGroup.querySelector('.js-checkbox-valid');
 
             checkboxes.forEach(checkbox => {
                 checkbox.addEventListener('change', () => {
-                    getCheckBoxGroups();
-                })
-            });
-            
-            group.querySelectorAll('.js-checkbox-valid').forEach(validator => {
-                validator.addEventListener('invalid', (e) => {
-                    e.preventDefault();
-
-                    const labelElm = group.querySelector('label');
-
-                    // Highlight label of checkbox group
-                    labelElm.classList.add('u-color__text--danger');
-                    labelElm.scrollIntoView({'block': 'center'});
+                    let validator = checkboxGroup.querySelectorAll('.c-option [type="checkbox"]:checked');
+                    if(validator.length > 0) {
+                        validationElement.setAttribute('checked', true);
+                        checkboxGroup.querySelector('label').classList.remove('u-color__text--danger');
+                    } else {
+                        validationElement.removeAttribute('checked');
+                    }
                 })
             })
         })
     }
 
-    /*
-    ** Initial check for checkboxes and add listeners
-    */
+/**
+ * Initiates the listener
+ */
     const init = () => {
-        getCheckBoxGroups();
         addListeners();
     }
 
