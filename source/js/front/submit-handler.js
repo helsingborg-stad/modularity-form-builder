59ff16dde8bd9353e11b3a165541e94f837daf83
NiclasNorin <niclas.norin@frontwalker.se>
2022-11-25 16:40:40 +0100

    I dont find any issues removing these.
    Seems like it was meant to further validate checkboxes. But this works as expected without these files. Also removing the submit event since the submit is being handled in field.js

--- source/js/front/submit-handler.js
+++
@@ -1,8 +0,0 @@
-export default (function ($) {
-    function Submit() {
-        $('form').submit(function (event) {
-            this.handleEvents();
-        }.bind(this));
-    }
-    return new Submit();
-})(jQuery);
