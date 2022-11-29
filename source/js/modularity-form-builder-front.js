298c3ba3b29ff7d87fa05d6e5a2eca67ce0fa0e7
NiclasNorin <niclas.norin@frontwalker.se>
2022-11-29 17:01:08 +0100

    Collapse moved to styleguide

--- source/js/modularity-form-builder-front.js
+++ source/js/modularity-form-builder-front.js
@@ -1,9 +1,7 @@
-import Collapse from './front/collapse';
 import GetLocation from './front/get-location';
 import HandleConditions from './front/handle-conditions';
 
 const FormBuilderFront = {
-  Collapse,
   GetLocation,
   HandleConditions,
 };
