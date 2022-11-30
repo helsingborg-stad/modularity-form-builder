fc1e58c18ec95dd8bb4efcd0ec6690dad538a7c0
NiclasNorin <103985736+NiclasNorin@users.noreply.github.com>
2022-11-30 13:26:34 +0100

    I dont find any issues removing these. (#70)
    * I dont find any issues removing these. 
    
    Seems like it was meant to further validate checkboxes. But this works as expected without these files. Also removing the submit event since the submit is being handled in field.js
    
    * Collapse moved to styleguide

--- source/js/modularity-form-builder-front.js
+++ source/js/modularity-form-builder-front.js
@@ -1,11 +1,7 @@
-import Collapse from './front/collapse';
 import GetLocation from './front/get-location';
 import HandleConditions from './front/handle-conditions';
-import SubmitHandler from './front/submit-handler';
 
 const FormBuilderFront = {
-  Collapse,
   GetLocation,
   HandleConditions,
-  SubmitHandler,
 };
