626d08f681331b02a34a35b54a9dd13f7dc536a0
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-08-31 11:43:56 +0200

    Autoload & Build

--- modularity-form-builder.php
+++ modularity-form-builder.php
@@ -14,7 +14,6 @@ define('FORM_BUILDER_MODULE_TEMPLATE_PATH', FORM_BUILDER_MODULE_PATH . 'template
 //Load lang
 load_plugin_textdomain('modularity-form-builder', false, plugin_basename(dirname(__FILE__)) . '/languages');
 
-require_once FORM_BUILDER_MODULE_PATH . 'source/php/Vendor/Psr4ClassLoader.php';
 require_once FORM_BUILDER_MODULE_PATH . 'Public.php';
 if (file_exists(FORM_BUILDER_MODULE_PATH . 'vendor/autoload.php')) {
     require_once FORM_BUILDER_MODULE_PATH . 'vendor/autoload.php';
@@ -30,13 +29,6 @@ add_action('plugins_loaded', function () {
     ));
     $acfExportManager->import();
 });
-
-// Instantiate and register the autoloader
-$loader = new ModularityFormBuilder\Vendor\Psr4ClassLoader();
-$loader->addPrefix('ModularityFormBuilder', FORM_BUILDER_MODULE_PATH);
-$loader->addPrefix('ModularityFormBuilder', FORM_BUILDER_MODULE_PATH . 'source/php/');
-$loader->register();
-
 
 // Start application
 if (function_exists('get_field')) {
