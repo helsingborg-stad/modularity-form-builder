f5188130ea70a0867f8b55de98dbd45f9bae5180
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-04-02 15:06:44 +0200

    fix: set width 100% on "accordion" expansions in forms. Add icon.

--- source/php/Module/views/fields/collapse.blade.php
+++ source/php/Module/views/fields/collapse.blade.php
@@ -1,7 +1,13 @@
 <div class="mod-form-collapse gutter gutter-top" {!! $field['conditional_hidden'] !!}>
     @button([
         'text' => $field['button_text'],
-        'color' => 'primary'
+        'color' => 'primary',
+        'icon' => 'expand_content',
+        'reversePositions' => true,
+        'classList' => [
+            'u-text-align--left',
+            'u-width--100'
+        ],
     ])
     @endbutton
 </div>
