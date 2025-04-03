26fdad6f84ec592c68a56c829c2318cfc4d63b4a
GitHub Action <>
2025-04-03 06:11:14 +0000

    build: update package file versions to 3.4.4

--- modularity-form-builder.php
+++ modularity-form-builder.php
@@ -3,7 +3,7 @@
  * Plugin Name: Modularity Form Builder
  * Plugin URI: http://github.com/helsingborg-stad
  * Description: Build submittable form(s) to display with module
- * Version: 3.4.3
+ * Version: 3.4.4
  * Author: Kristoffer Svanmark, Sebastian Thulin
  */
 
