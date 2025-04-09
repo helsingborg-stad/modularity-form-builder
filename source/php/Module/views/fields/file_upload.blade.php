74b295e2f0b271d854f603745475559b723b9749
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-04-09 14:20:19 +0200

    fix: remove deprecated params, add label

--- source/php/Module/views/fields/file_upload.blade.php
+++ source/php/Module/views/fields/file_upload.blade.php
@@ -7,8 +7,8 @@
                     ? ModularityFormBuilder\Helper\SanitizeData::convertLinks($field['description'])
                     : '',
                 'buttonLabel' => $field['button_label'],
+                'buttonRemoveLabel' => $field['button_remove_label'] ?? '';
                 'required' => $field['required'] ? true : false,
-                'display' => 'area',
                 'multiple' => $field['type'] === 'multiple' ? true : false,
                 'label' => $field['label'],
                 'accept' => $field['filetypes'] && is_array($field['filetypes']) ? $field['attributeList']['accept'] : '',
